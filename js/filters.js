792d8590d2060918bae978ee72fa8289f6e83426
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-02-12 18:47:05 +0300

    Вынесла фильтры в отдельный фильтр

---
+++ js/filters.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+  // Работаем с изображениями на форме
+  var editingWindow = document.querySelector('.img-upload');
+  var editingWindowFilters = editingWindow.querySelector('.img-upload__preview img');
+  var filtersList = editingWindow.querySelector('.effects');
+
+// Работаем с изображениями на форме
+// Добавление фильтра к картинке по клику
+  filtersList.addEventListener('click', setFilter);
+
+  function setFilter(evt) {
+    if (evt.target.checked) {
+      editingWindowFilters.className = 'effects__preview--' + evt.target.value;
+    }
+  }
+})();
