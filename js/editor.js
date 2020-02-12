d5e778c98f412e452883bd1ec872c2430e7c900f
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-02-12 18:44:21 +0300

    Модуляция редактора

--- js/editor.js
+++ js/editor.js
@@ -1,81 +1,63 @@
 'use strict';
 
-// Для формы редактирования загруженной фотографии
-// Открываем и закрываем форму
-var editingWindow = document.querySelector('.img-upload');
-var editingWindowComment = editingWindow.querySelector('.text__description');
-var fileUploadButton = editingWindow.querySelector('.img-upload__input');
-var previewWindow = editingWindow.querySelector('.img-upload__overlay');
-var editingWindowHashtags = editingWindow.querySelector('.text__hashtags');
+(function () {
+  // Для формы редактирования загруженной фотографии
+  var galleryOverlay = document.querySelector('body');
 
-var closePreviewWindowBtn = editingWindow.querySelector('.img-upload__cancel');
-var submitPhotoBtn = editingWindow.querySelector('.img-upload__submit');
+  var editingWindow = document.querySelector('.img-upload');
+  var fileUploadButton = editingWindow.querySelector('.img-upload__input');
+  var previewWindow = editingWindow.querySelector('.img-upload__overlay');
+  var closePreviewWindowBtn = editingWindow.querySelector('.img-upload__cancel');
+  var submitPhotoBtn = editingWindow.querySelector('.img-upload__submit');
+  var editingWindowFilters = editingWindow.querySelector('.img-upload__preview img');
 
-fileUploadButton.addEventListener('change', openEditingWindow);
+  var pictureZoomingValue = editingWindow.querySelector('.scale__control--value');
+  var editingWindowHashtags = editingWindow.querySelector('.text__hashtags');
+  var editingWindowComment = editingWindow.querySelector('.text__description');
 
-// Закрываем окно редактирования фотографий
-function closeEditingWindow() {
-  window.utils.addClassName(previewWindow, 'hidden');
-  window.utils.removeClassName(galleryOverlay, 'modal-open');
-  // удаляем обработчик закрытия окна
-  closePreviewWindowBtn.removeEventListener('click', closeEditingWindow);
-  // удаляем обработчик закрытия окна по кноаке отправить
-  submitPhotoBtn.removeEventListener('submit', closeEditingWindow);
-  // удаляем обработчик закрытия окна по клавише ESC
-  document.removeEventListener('keydown', onEditingWindowKeyDown);
-}
+  var currentZoomValue = 1;
 
-// Функция закрытия окна редактирования фото по клику на ESC
-function onEditingWindowKeyDown() {
-  if (document.activeElement !== editingWindowHashtags && document.activeElement !== editingWindowComment) {
-    closeEditingWindow();
+  fileUploadButton.addEventListener('change', openEditingWindow);
+
+  // Открываем окно редактирования фотографий
+  function openEditingWindow() {
+    resetFilters();
+
+    window.utils.addClassName(galleryOverlay, 'modal-open');
+    window.utils.removeClassName(previewWindow, 'hidden');
+
+    // добавляем обработчик закрытия окна
+    closePreviewWindowBtn.addEventListener('click', closeEditingWindow);
+    // добавляем обработчик закрытия окна по кнопке отправить
+    submitPhotoBtn.addEventListener('submit', closeEditingWindow);
+    // добавляем обработчик закрытия окна по клавише ESC
+    document.addEventListener('keydown', onEditingWindowKeyDown);
   }
-}
 
-
-
-var DataScale = {
-  STEP_RESIZE: 0.25,
-  MIN_ZOOM: 0.25,
-  MAX_ZOOM: 1
-};
-
-var currentZoomValue = 1;
-
-// Работаем с изображениями на форме
-var editingWindowFilters = editingWindow.querySelector('.img-upload__preview img');
-var filtersList = editingWindow.querySelector('.effects');
-
-function resetFilters() {
-  editingWindowComment.value = '';
-  editingWindowHashtags.value = '';
-  editingWindowFilters.className = 'effects__preview--none';
-  pictureZoomingValue.value = currentZoomValue * 100 + '%';
-}
-
-// Открываем окно редактирования фотографий
-function openEditingWindow() {
-  resetFilters();
-
-  window.utils.addClassName(galleryOverlay, 'modal-open');
-  window.utils.removeClassName(previewWindow, 'hidden');
-
-  // добавляем обработчик закрытия окна
-  closePreviewWindowBtn.addEventListener('click', closeEditingWindow);
-  // добавляем обработчик закрытия окна по кнопке отправить
-  submitPhotoBtn.addEventListener('submit', closeEditingWindow);
-  // добавляем обработчик закрытия окна по клавише ESC
-  document.addEventListener('keydown', onEditingWindowKeyDown);
-}
-
-// Работаем с изображениями на форме
-// Добавление фильтра к картинке по клику
-filtersList.addEventListener('click', setFilter);
-
-function setFilter(evt) {
-  if (evt.target.checked) {
-    editingWindowFilters.className = 'effects__preview--' + evt.target.value;
+  function resetFilters() {
+    editingWindowComment.value = '';
+    editingWindowHashtags.value = '';
+    editingWindowFilters.className = 'effects__preview--none';
+    pictureZoomingValue.value = currentZoomValue * 100 + '%';
   }
-}
 
+  // Функция закрытия окна редактирования фото по клику на ESC
+  function onEditingWindowKeyDown() {
+    if (document.activeElement !== editingWindowHashtags && document.activeElement !== editingWindowComment) {
+      closeEditingWindow();
+    }
+  }
+
+  // Закрываем окно редактирования фотографий
+  function closeEditingWindow() {
+    window.utils.addClassName(previewWindow, 'hidden');
+    window.utils.removeClassName(galleryOverlay, 'modal-open');
+    // удаляем обработчик закрытия окна
+    closePreviewWindowBtn.removeEventListener('click', closeEditingWindow);
+    // удаляем обработчик закрытия окна по кноаке отправить
+    submitPhotoBtn.removeEventListener('submit', closeEditingWindow);
+    // удаляем обработчик закрытия окна по клавише ESC
+    document.removeEventListener('keydown', onEditingWindowKeyDown);
+  }
+})();
 
