247cd3b5d81f461592cdb96c5cd1a874ecc9d2f3
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-03-14 13:22:32 +0300

    Добавила фильтрация обсуждаемых фотографий

--- js/start.js
+++ js/start.js
@@ -2,6 +2,21 @@
 
 (function () {
   var URL = 'https://js.dump.academy/kekstagram/data';
+
+  var sortImages = document.querySelector('.img-filters');
+  var defaultPhotosButtonSort = sortImages.querySelector('#filter-default');
+  var randomPhotosButtonSort = sortImages.querySelector('#filter-random');
+  var discussedPhotosButtonSort = sortImages.querySelector('#filter-discussed');
+
+  function getRank(photos) {
+    var commentsLength  = photos.map(function (it) {
+      return it.comments.length;
+    });
+    return commentsLength;
+  }
+
+
+
   function onError(message) {
     window.utils.renderInfoMessage('#error', '.error', message);
   }
@@ -13,6 +28,15 @@
     // Показываем фотки
     window.gallery.generatePhotos(photos);
     window.editor.uploadPhoto();
+    window.utils.removeClassName(sortImages, 'img-filters--inactive');
+
+    console.log(getRank(photos))
+
+    discussedPhotosButtonSort.addEventListener('click', function (evt) {
+      console.log(photos.sort(function (left, right) {
+        return getRank(right) - getRank(left);
+      }));
+    })
   }
 
   window.backend.load(URL, 'GET', onSuccess, onError);
