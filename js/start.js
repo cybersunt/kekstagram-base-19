3324f77e3cb8dadf4bd1a7e357b47c0f136778e8
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-03-14 16:14:11 +0300

    Изменила функцию сортировки массива

--- js/start.js
+++ js/start.js
@@ -8,15 +8,6 @@
   var randomPhotosButtonSort = sortImages.querySelector('#filter-random');
   var discussedPhotosButtonSort = sortImages.querySelector('#filter-discussed');
 
-  function getRank(photos) {
-    var commentsLength  = photos.map(function (it) {
-      return it.comments.length;
-    });
-    return commentsLength;
-  }
-
-
-
   function onError(message) {
     window.utils.renderInfoMessage('#error', '.error', message);
   }
@@ -30,12 +21,48 @@
     window.editor.uploadPhoto();
     window.utils.removeClassName(sortImages, 'img-filters--inactive');
 
-    console.log(getRank(photos))
+    defaultPhotosButtonSort.addEventListener('click', function () {
+      window.data.savePhotos(data);
+      var photos = window.data.getCurrentData();
+      console.log(window.gallery.generatePhotos(photos));
+    })
+    //
+    // var photosCommentsLength = photos.map(function (photos) {
+    //   return photos.comments.length;
+    // });
+    //
+    // console.log(photosCommentsLength);
 
-    discussedPhotosButtonSort.addEventListener('click', function (evt) {
-      console.log(photos.sort(function (left, right) {
-        return getRank(right) - getRank(left);
-      }));
+   //  discussedPhotosButtonSort.addEventListener('click', function (evt) {
+   //    console.log(photos.map(function (photos, index) {
+   //      return { index: i, commentsLength: photos.commenst };
+   //    }).sort(function (a, b) {
+   //      return b - a;
+   //    }));
+   // })
+
+    discussedPhotosButtonSort.addEventListener('click', function () {
+      var mapped = photos.map(function(el, i) {
+        return { index: i, value: el.comments.length };
+      });
+
+      console.log(mapped);
+
+// сортируем массив, содержащий уменьшенные значения
+      mapped.sort(function(a, b) {
+        if (a.value > b.value) {
+          return -1; }
+        if (a.value < b.value) {
+          return 1; }
+        return 0;
+      });
+
+// контейнер для результа
+      var result = mapped.map(function(el) {
+        return photos[el.index];
+      });
+
+      console.log(result);
     })
   }
 
