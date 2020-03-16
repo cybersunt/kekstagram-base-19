bf83c02c938a232ec356d34fedd0ae2df69c18e2
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-03-16 15:33:20 +0300

    Устранение ошибок по передаче данных в функции

--- js/sorting.js
+++ js/sorting.js
@@ -1,82 +1,72 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/kekstagram/data';
-
   var sortImages = document.querySelector('.img-filters');
   var defaultPhotosButtonSort = sortImages.querySelector('#filter-default');
   var randomPhotosButtonSort = sortImages.querySelector('#filter-random');
   var discussedPhotosButtonSort = sortImages.querySelector('#filter-discussed');
 
-  function onError(message) {
-    window.utils.renderInfoMessage('#error', '.error', message);
-  }
-
-  function onSuccess(data) {
-    window.data.savePhotos(data);
+  function sortByDefault() {
     var photos = window.data.getCurrentData();
 
-    function sortByDefault() {
-      window.gallery.removePhotos();
-      window.gallery.renderPhotos(photos);
-    }
-
-    function sortByDiscussedPhotos() {
-      var discussedPhotos = getDiscussedPhotos();
-      window.gallery.removePhotos();
-      window.gallery.renderPhotos(discussedPhotos);
-    }
-
-    function sortBySomeRandomPhotos() {
-      var randomPhotos = getSomeRandomPhotos();
-      window.gallery.removePhotos();
-      window.gallery.renderPhotos(randomPhotos);
-    }
-
-    function getDiscussedPhotos() {
-      var mappedArray = photos.map(function (el, i) {
-        return {index: i, value: el.comments.length};
-      });
-
-      mappedArray.sort(function (a, b) {
-        return b.value - a.value;
-      });
-
-      var discussedPictures = mappedArray.map(function (el) {
-        return photos[el.index];
-      });
-
-      return discussedPictures;
-    }
-
-    function getSomeRandomPhotos() {
-      var someRandomPhotos = photos.map(function (elem) {
-        return [elem, Math.random()];
-      })
-        .sort(function (a, b) {
-          return a[1] - b[1];
-        })
-        .map(function (elem) {
-          return elem[0];
-        });
-      someRandomPhotos.length = window.constants.MAX_LENGTH_GALLERY;
-
-      return someRandomPhotos;
-    }
-
-    var sortPhoto = function () {
-      window.utils.removeClassName(sortImages, 'img-filters--inactive');
-
-      defaultPhotosButtonSort.addEventListener('click', sortByDefault);
-      discussedPhotosButtonSort.addEventListener('click', sortByDiscussedPhotos);
-      randomPhotosButtonSort.addEventListener('click', sortBySomeRandomPhotos);
-    };
-
-    window.sorting = {
-      sortPhoto: sortPhoto
-    };
+    window.gallery.removePhotos();
+    window.gallery.renderPhotos(photos);
   }
 
-  window.backend.load(URL, 'GET', onSuccess, onError);
+  function sortByDiscussedPhotos() {
+    var photos = window.data.getCurrentData();
+    var discussedPhotos = getDiscussedPhotos(photos);
+    window.gallery.removePhotos();
+    window.gallery.renderPhotos(discussedPhotos);
+  }
 
+  function sortBySomeRandomPhotos() {
+    var photos = window.data.getCurrentData();
+    var randomPhotos = getSomeRandomPhotos(photos);
+    window.gallery.removePhotos();
+    window.gallery.renderPhotos(randomPhotos);
+  }
+
+  function getDiscussedPhotos(photos) {
+    var mappedArray = photos.map(function (el, i) {
+      return {index: i, value: el.comments.length};
+    });
+
+    mappedArray.sort(function (a, b) {
+      return b.value - a.value;
+    });
+
+    var discussedPictures = mappedArray.map(function (el) {
+      return photos[el.index];
+    });
+
+    return discussedPictures;
+  }
+
+  function getSomeRandomPhotos(photos) {
+    var someRandomPhotos = photos.map(function (elem) {
+      return [elem, Math.random()];
+    })
+      .sort(function (a, b) {
+        return a[1] - b[1];
+      })
+      .map(function (elem) {
+        return elem[0];
+      });
+    someRandomPhotos.length = window.constants.MAX_LENGTH_GALLERY;
+
+    return someRandomPhotos;
+  }
+
+  var sortPhoto = function () {
+    window.utils.removeClassName(sortImages, 'img-filters--inactive');
+
+    defaultPhotosButtonSort.addEventListener('click', window.debounce(sortByDefault));
+    discussedPhotosButtonSort.addEventListener('click', window.debounce(sortByDiscussedPhotos));
+    randomPhotosButtonSort.addEventListener('click', window.debounce(sortBySomeRandomPhotos));
+  };
+
+  window.sorting = {
+    sortPhoto: sortPhoto
+  };
 })();
